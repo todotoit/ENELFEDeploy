6a54f87fa417f24b806f25cfca41be7abe819299
abusedmedia <ff@todo.to.it>
2017-06-09 21:18:57 +0200

    webapp

--- app/bower_components/stateman/src/index.js
+++
@@ -1,7 +0,0 @@
-
-var StateMan = require("./stateman.js");
-StateMan.Histery = require("./histery.js");
-StateMan.util = require("./util.js");
-StateMan.State = require("./state.js");
-
-module.exports = StateMan;
