04139504d6a7ae3f51735b002c379a4ec67f7bf6
marcoaimo <marcoaimo.am@gmail.com>
2017-07-15 14:20:53 +0200

    deploy

--- js/components/snippetCard/component.js
+++ js/components/snippetCard/component.js
@@ -23,8 +23,8 @@
     var ctrl = this
     // https://github.com/angular/angular.js/issues/14433
     // for the issue above we decided to use just $onChanges
-    ctrl.$onInit = init
-    // ctrl.$onChanges = update
+    // ctrl.$onInit = init
+    ctrl.$onChanges = init
 
     $scope.prevTab  = prevTab
     $scope.nextTab  = nextTab
@@ -49,23 +49,65 @@
       return idx === contentIdx
     }
 
-    function prevTab() {
-      if (contentIdx <= 0) return prevCallback()
-      contentIdx--
-      $scope.subsnip = content[contentIdx]
-      // $content.find('li').removeClass('active')
-      // $content.find('li').eq(contentIdx).addClass('active')
-      TweenMax.to($content.find('ul'), swipeVel, { x: '+='+ swipeOffset +'%', onComplete: function() {
+    // function prevTab() {
+    //   if (contentIdx <= 0) return prevCallback()
+    //   contentIdx--
+    //   $scope.subsnip = content[contentIdx]
+    //   TweenMax.to($content.find('ul'), swipeVel, { x: '+='+ swipeOffset +'%', onComplete: function() {
+    //     if (!$scope.$$phase) $scope.$digest()
+    //   } })
+    //   TweenMax.to($content.find('.sub-snip-content'), swipeVel, { x: '+='+ swipeOffset +'%', opacity: 0.1, onComplete: function() {
+    //     if (!$scope.$$phase) $scope.$digest()
+    //   } })
+    // }
+    // function nextTab() {
+    //   if (contentIdx >= content.length -1) return nextCallback()
+    //   contentIdx++
+    //   $scope.subsnip = content[contentIdx]
+    //   TweenMax.to($content.find('ul'), swipeVel, { x: '-='+ swipeOffset +'%', onComplete: function() {
+    //     if (!$scope.$$phase) $scope.$digest()
+    //   } })
+    //   TweenMax.to($content.find('.sub-snip-content'), swipeVel, { x: '-='+ swipeOffset +'%', opacity: 0.1, onComplete: function() {
+    //     if (!$scope.$$phase) $scope.$digest()
+    //   } })
+    // }
+
+    function prevTab(digest){
+        if (contentIdx <= 0) return prevCallback()
+        contentIdx--
+        $scope.subsnip = content[contentIdx]
+        setTimeout(scrollToCurrent, 100)
+        if (!digest) return
         if (!$scope.$$phase) $scope.$digest()
-      } })
     }
-    function nextTab() {
-      if (contentIdx >= content.length -1) return nextCallback()
-      contentIdx++
-      $scope.subsnip = content[contentIdx]
-      TweenMax.to($content.find('ul'), swipeVel, { x: '-='+ swipeOffset +'%', onComplete: function() {
+
+    function nextTab(digest){
+        if (contentIdx >= content.length -1) return nextCallback()
+        contentIdx++
+        $scope.subsnip = content[contentIdx]
+        setTimeout(scrollToCurrent, 100)
+        if (!digest) return
         if (!$scope.$$phase) $scope.$digest()
-      } })
+    }
+
+    function scrollToCurrent(){
+      var current = $element.find('.note.active')
+      var container = $element.find('ul.sub-snip-nav')
+      var offset = getScrollOffset(current, container)
+      var navs = container.find('.note');
+      console.log(offset)
+      TweenMax.to(navs, .5, {x: offset})
+    }
+
+    function getScrollOffset(current, container){
+      var idx = current.index();
+      console.log(current, idx, current.position(), current.outerWidth(), container.outerWidth())
+      if(idx > (container.children().length-1) / 2){
+        var offset = container.outerWidth() - current.position().left - current.outerWidth();
+      } else {
+        var offset = 0;
+      }
+      return offset;
     }
 
     // init after dom loaded
@@ -78,7 +120,7 @@
       if (contentIdx < 0) contentIdx = 0
       else if (contentIdx >= content.length) contentIdx = content.length -1
       $scope.subsnip = content? content[contentIdx] : null
-      if(!bowser.mobile) $element.ready(createContentHandler)
+      $element.ready(createContentHandler)
     }
 
     // event handlers
@@ -87,18 +129,17 @@
       if (contentIdx !== 0) TweenMax.set($content.find('li'), { x: '-='+ (swipeOffset * contentIdx) +'%' })
       hammertime = new Hammer($content[0], { domEvents: true, css_hacks:false, touchAction: 'compute' })
       hammertime.on('swipeleft',  function(){
-        nextTab()
+        nextTab(true)
       })
-      hammertime.on('swiperight', prevTab)
+      hammertime.on('swiperight', function() {
+        prevTab(true)
+      })
       hammertime.on('hammer.input', function (e) {
-        e.preventDefault()
         e.srcEvent.stopPropagation()
       })
       $element.on('touchmove', function(e) {
-        if(!bowser.mobile){
-          e.stopPropagation()
-          e.preventDefault()
-        }
+        e.stopPropagation()
+        e.preventDefault()
       })
       $element.click(function(e) {
         e.stopPropagation()
@@ -107,8 +148,8 @@
     }
 
     function navigateTo(index){
-      if(contentIdx > index) prevTab()
-      else nextTab();
+      if(contentIdx > index) prevTab(false)
+      else nextTab(false);
     }
 
     // deregister event handlers
